01e229b099b4ba3d385bd6793b113403055ccd36
unknown <nh229u07@NH229C07.csd.bit>
2015-02-13 13:15:10 -0600

    Bryan Hurst EventType Object

---
+++ com.WanderingTurtle/com.WanderingTurtle.Common/EventType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class EventType
+{
+    public int EventTypeID     { get; set; }
+    public string EventName    { get; set; }
+
+    public EventType(int eventTypeID, string eventName)
+	{
+        EventTypeID = eventTypeID;
+        EventName = eventName;
+	}
+}
