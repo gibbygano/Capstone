88355a57588885d47f05491b9def666b0f89957a
Miguel Santana <santana1053@gmail.com>
2015-02-16 14:07:43 -0600

    Remove HotelGuestPIN

--- com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spHotelGuestGetList.sql
+++ com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spHotelGuestGetList.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[spHotelGuestGetList]
 AS
 BEGIN
-    SELECT [HotelGuestID],[FirstName],[LastName],[Zip],[Address1],[Address2],[PhoneNumber],[EmailAddress],[HotelGuestPIN]
+    SELECT [HotelGuestID],[FirstName],[LastName],[Zip],[Address1],[Address2],[PhoneNumber],[EmailAddress]
     FROM [dbo].[HotelGuest]
 
 END
