3ce244e4ba14d4ffb34f8cd06cee50b88b5404eb
Miguel Santana <santana1053@gmail.com>
2015-02-19 13:56:34 -0600

    AddEditHotelGuest Zip Code ComboBox
    Changed the way the cboZip displays the zip code to the user, added
    State and City associated with the Zip

--- com.WanderingTurtle/com.WanderingTurtle.Common/CityState.cs
+++ com.WanderingTurtle/com.WanderingTurtle.Common/CityState.cs
@@ -12,17 +12,19 @@ namespace com.WanderingTurtle.Common
     /// </summary>
     public class CityState
     {
-        public string Zip { get; set; }
-
-        public string City { get; set; }
-
-        public string State { get; set; }
-
         public CityState(string zip, string city, string state)
         {
             this.Zip = zip;
             this.City = city;
             this.State = state;
         }
+
+        public string City { get; set; }
+
+        public string GetZipStateCity { get { return String.Format("{1}{0}{2}{0}{3}", " - ", this.Zip, this.State, this.City); } }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
     }
 }
