bb78db2bb325de2404c0dc032aaa3d78af997785
Hunter <hunterslind@gmail.com>
2015-02-19 15:04:28 -0800

    Added a lot of usability functions

--- com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spInsertEventItem.sql
+++ com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spInsertEventItem.sql
@@ -3,17 +3,15 @@
 	@EventItemName 			varchar(255), 
 	@EventStartTime 		datetime2, 
 	@EventEndTime 			datetime2, 
-	@CurrentNumberOfGuests 	int, 
+	@CurrentNumberOfGuests 	int = 0, 
 	@MaxNumberOfGuests 		int, 
 	@MinNumberOfGuests 		int, 
 	@EventTypeID 			int, 
 	@PricePerPerson 		money, 
 	@EventOnsite 			bit, 
 	@Transportation 		bit, 
-	@EventDescription 		varchar(255), 
-	@EventItemID 			int OUTPUT)
+	@EventDescription 		varchar(255))
 AS
 INSERT INTO EventItem(EventItemName, EventStartTime, EventEndTime, CurrentNumberOfGuests, MaxNumberOfGuests, MinNumberOfGuests, EventTypeID, PricePerPerson, EventOnsite, Transportation, EventDescription, Active) 
 VALUES (@EventItemName, @EventStartTime, @EventEndTime, @CurrentNumberOfGuests, @MaxNumberOfGuests, @MinNumberOfGuests, @EventTypeID, @PricePerPerson, @EventOnsite, @Transportation, @EventDescription, 1)
-SET @EventItemID = SCOPE_IDENTITY()
-RETURN @@ERROR
+RETURN @@ROWCOUNT
