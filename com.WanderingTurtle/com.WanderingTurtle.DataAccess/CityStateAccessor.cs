c3104558f57d2ea7d36936282a85186aed1bd51e
Miguel Santana <santana1053@gmail.com>
2015-02-18 22:04:23 -0600

    Add CityState Data Access

---
+++ com.WanderingTurtle/com.WanderingTurtle.DataAccess/CityStateAccessor.cs
@@ -0,0 +1,54 @@
+﻿using com.WanderingTurtle.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace com.WanderingTurtle.DataAccess
+{
+    public class CityStateAccessor
+    {
+        public static List<CityState> CityStateGetList(String zip = null)
+        {
+            SqlConnection conn = DatabaseConnection.GetDatabaseConnection();
+
+            String cmdText = (zip == null) ? "spCityStateReadAll" : "spCityStateRead";
+            SqlCommand cmd = new SqlCommand(cmdText, conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (zip != null) { cmd.Parameters.AddWithValue("@zip", zip); }
+
+            List<CityState> list = new List<CityState>();
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new CityState(
+                            reader.GetString(0), //Zip
+                            reader.GetString(1), //City
+                            reader.GetString(2) //State
+                        ));
+                    }
+                }
+                else
+                {
+                    throw new ApplicationException(zip == null ? "Did not find any records for CityState" : "Could not find CityState record for specified Zip");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return list;
+        }
+    }
+}
---
+++ com.WanderingTurtle/com.WanderingTurtle/CityStateManager.cs
@@ -0,0 +1,48 @@
+﻿using com.WanderingTurtle.Common;
+using com.WanderingTurtle.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.WanderingTurtle.BusinessLogic
+{
+    public class CityStateManager
+    {
+        /// <summary>
+        /// Get a list of all CityState records
+        /// </summary>
+        /// <returns>Return List of CityState Objects</returns>
+        /// Miguel Santana 2/18/2015
+        public List<CityState> GetCityStateList()
+        {
+            try
+            {
+                return CityStateAccessor.CityStateGetList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Get a CityState record by zip
+        /// </summary>
+        /// <param name="zip">Specificed Zip to look up</param>
+        /// <returns>Return CityState Object</returns>
+        /// Miguel Santana 2/18/2015
+        public CityState GetCityState(String zip)
+        {
+            try
+            {
+                List<CityState> list = CityStateAccessor.CityStateGetList();
+                return (list.Count == 1) ? list.ElementAt(0) : null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
