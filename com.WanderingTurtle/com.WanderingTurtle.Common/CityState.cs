0e82f91b49ddb107773acc442be1107357872fe4
Miguel Santana <santana1053@gmail.com>
2015-02-18 20:08:31 -0600

    Update CityState Object
    Removed unused default constructor

--- com.WanderingTurtle/com.WanderingTurtle.Common/CityState.cs
+++ com.WanderingTurtle/com.WanderingTurtle.Common/CityState.cs
@@ -7,26 +7,22 @@ namespace com.WanderingTurtle.Common
 {
     /// <summary>
     /// City State object populated with data from the CityState lookup table
-    /// 
+    ///
     /// Created by Matt Lapka - 2/8/15
     /// </summary>
     public class CityState
     {
         public string Zip { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
 
-        public CityState()
-        {
-            //default constructor
-        }
+        public string City { get; set; }
+
+        public string State { get; set; }
 
         public CityState(string zip, string city, string state)
         {
-            Zip = zip;
-            City = city;
-            State = state;
-
+            this.Zip = zip;
+            this.City = city;
+            this.State = state;
         }
     }
 }
