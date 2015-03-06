96a81d7b2b2eef42fb2e08f46c4dee1b5f42f1db
Pat Banks <patbanks96@gmail.com>
2015-03-05 18:51:16 -0600

    Updates to invoice stored procedures

--- com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spSelectInvoiceByGuest.sql
+++ com.WanderingTurtle/com.WanderingTurtle.EventDatabase/dbo/Stored Procedures/spSelectInvoiceByGuest.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE spSelectInvoiceByGuest
 	(@guestID int)
 AS
 BEGIN
-	SELECT InvoiceID, Invoice.HotelGuestID, DateOpened, Invoice.Active, HotelGuest.LastName, HotelGuest.FirstName, HotelGuest.Room
+	SELECT InvoiceID, Invoice.HotelGuestID, DateOpened, Invoice.Active, Invoice.DateClosed, TotalPaid, HotelGuest.LastName, HotelGuest.FirstName, HotelGuest.Room
 	FROM Invoice, HotelGuest
 	WHERE Invoice.HotelGuestID = @guestID
 	AND Invoice.HotelGuestID = HotelGuest.HotelGuestID
