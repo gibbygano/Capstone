13305a694541239f75a72dcbac49692208e97a15
Miguel Santana <santana1053@gmail.com>
2015-03-08 18:31:03 -0500

    Working Screen Switching

--- com.WanderingTurtle/com.WanderingTurtle.FormPresentation/MainWindow.xaml.cs
+++ com.WanderingTurtle/com.WanderingTurtle.FormPresentation/MainWindow.xaml.cs
@@ -8,43 +8,44 @@ using SplashScreen = com.WanderingTurtle.FormPresentation.Views.SplashScreen;
 
 namespace com.WanderingTurtle.FormPresentation
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow
-	{
-		public MainWindow()
-		{
-			try
-			{
-				ConnectionManager.TestConnection();
-			}
-			catch (Exception ex)
-			{
-				switch (Xceed.Wpf.Toolkit.MessageBox.Show(
-					string.Format("Error connecting to database.\rWould you like to exit the program?\r\rError:\r{0}", ex.Message),
-					"Could not connect to the database", MessageBoxButton.YesNo, MessageBoxImage.Error))
-				{
-					case MessageBoxResult.Yes:
-						Environment.Exit(0);
-						break;
-				}
-			}
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            try
+            {
+                ConnectionManager.TestConnection();
+            }
+            catch (Exception ex)
+            {
+                switch (Xceed.Wpf.Toolkit.MessageBox.Show(
+                    string.Format("Error connecting to database.\rWould you like to exit the program?\r\rError:\r{0}", ex.Message),
+                    "Could not connect to the database", MessageBoxButton.YesNo, MessageBoxImage.Error))
+                {
+                    case MessageBoxResult.Yes:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            InitializeComponent();
+            StartUp();
+        }
 
-		private void BtnSignInOut_Click(object sender, RoutedEventArgs e)
-		{
-			StartUp();
-			//foreach (UIElement child in ((Grid)this.Content).Children)
-			//{
-			//    ((Grid)this.Content).Children.Remove(child);
-			//}
-		}
+        private void BtnSignInOut_Click(object sender, RoutedEventArgs e)
+        {
+            StartUp();
+            //foreach (UIElement child in ((Grid)this.Content).Children)
+            //{
+            //    ((Grid)this.Content).Children.Remove(child);
+            //}
+        }
 
-		internal void StartUp()
-		{
-			this.MainContent.Content = new SplashScreen();
-		}
-	}
+        internal void StartUp()
+        {
+            this.MainContent.Content = new SplashScreen();
+        }
+    }
 }
--- com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Views/SplashScreen.xaml.cs
+++ com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Views/SplashScreen.xaml.cs
@@ -3,19 +3,19 @@ using System.Windows;
 
 namespace com.WanderingTurtle.FormPresentation.Views
 {
-	/// <summary>
-	/// Interaction logic for SplashScreen.xaml
-	/// </summary>
-	public partial class SplashScreen
-	{
-		public SplashScreen()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
 
-		private void BtnSignIn_Click(object sender, RoutedEventArgs e)
-		{
-			WindowHelper.GetMainWindow(this).MainContent = new TabContainer();
-		}
-	}
+        private void BtnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHelper.GetMainWindow(this).MainContent.Content = new TabContainer();
+        }
+    }
 }
