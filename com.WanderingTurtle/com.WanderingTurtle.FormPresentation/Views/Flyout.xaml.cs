cbe090b4dded101ab49d914646594cbd7aa079e1
Miguel Santana <santana1053@gmail.com>
2015-03-09 14:00:29 -0500

    Remove SimpleChildWindow Package
    Update MahApps.Metro to prerelease

---
+++ com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Views/Flyout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace com.WanderingTurtle.FormPresentation.Views
+{
+    /// <summary>
+    /// Interaction logic for Flyout.xaml
+    /// </summary>
+    public partial class Flyout : IDisposable
+    {
+        public Flyout()
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
