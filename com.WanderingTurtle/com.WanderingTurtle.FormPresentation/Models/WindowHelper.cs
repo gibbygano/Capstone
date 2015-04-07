c546e050addefd4130500a17c5008f5a3ef28c88
Miguel Santana <santana1053@gmail.com>
2015-04-06 20:11:53 -0500

    WindowHelper - MakeReadOnly and SetStyle
    MakeReadOnly: Take any window or container and disable all the controls on them. Will take any control with a base class of Panel and loop through all the child contents, including sub containers.
    SetStyle: Simple yet complicated.  Using default parameters, sets the style almost as easily as you could do it outside the method. However, adding that optional parameter "replace" will only set the property if it already exists in the style.
    Also, Added Comments!

--- com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Models/WindowHelper.cs
+++ com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Models/WindowHelper.cs
@@ -1,13 +1,24 @@
 ﻿using MahApps.Metro.Controls;
 using System;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 
 namespace com.WanderingTurtle.FormPresentation.Models
 {
     internal static class WindowHelper
     {
-        public static MainWindow GetMainWindow(Control control)
+        /// <summary>
+        /// Returns the base parent <typeparamref name="MainWindow"/>
+        /// </summary>
+        /// <remarks>
+        /// Miguel Santana 2015/03/10
+        /// </remarks>
+        /// <param name="control"></param>
+        /// <returns>Base Parent <typeparamref name="MetroWindow"/></returns>
+        internal static MainWindow GetMainWindow(Control control)
         {
             var parent = VisualTreeHelper.GetParent(control) ?? control;
             while (!(parent is MainWindow))
@@ -17,7 +28,15 @@ namespace com.WanderingTurtle.FormPresentation.Models
             return (parent as MainWindow);
         }
 
-        public static MetroWindow GetWindow(Control control)
+        /// <summary>
+        /// Returns the parent <typeparamref name="MetroWindow"/> of any child control
+        /// </summary>
+        /// <remarks>
+        /// Miguel Santana 2015/03/10
+        /// </remarks>
+        /// <param name="control">The control that you wish to find the parent of</param>
+        /// <returns>Parent <typeparamref name="MetroWindow"/></returns>
+        internal static MetroWindow GetWindow(Control control)
         {
             var parent = VisualTreeHelper.GetParent(control);
             if (parent == null)
@@ -28,6 +47,86 @@ namespace com.WanderingTurtle.FormPresentation.Models
                 { if (parent != null) { parent = VisualTreeHelper.GetParent(parent); } }
             }
             return (parent as MetroWindow);
+        }
+
+        /// <summary>
+        /// Takes the child components of <paramref name="content"/> and disables them.
+        /// </summary>
+        /// <remarks>
+        /// Miguel Santana 2015/06/04
+        /// </remarks>
+        /// <param name="content">The parent container</param>
+        /// <param name="controlsToKeepEnabled">Controls that you want to keep enabled</param>
+        internal static void MakeReadOnly(Panel content, FrameworkElement[] controlsToKeepEnabled = null)
+        {
+            try
+            {
+                foreach (FrameworkElement child in content.Children)
+                {
+                    if (controlsToKeepEnabled != null && controlsToKeepEnabled.Contains(child))
+                    { return; }
+
+                    if (child is Panel)
+                    { MakeReadOnly(child as Panel, controlsToKeepEnabled); }
+                    else
+                    {
+                        if (child is TextBoxBase) { (child as TextBoxBase).IsReadOnly = true; }
+                        else { child.IsEnabled = false; }
+
+                        SetStyle(child, new Setter(TextBoxHelper.ClearTextButtonProperty, false));
+                    }
+                }
+            }
+            catch (Exception ex) { throw new Exception("Error Setting Fields to ReadOnly for" + Environment.NewLine + content, ex); }
+        }
+
+        /// <summary>
+        /// Sets the style of <paramref name="control"/> to <paramref name="setterValue"/>
+        /// </summary>
+        /// <remarks>
+        /// Miguel Santana 2015/04/06
+        /// </remarks>
+        /// <param name="control">The control to set the style of</param>
+        /// <param name="setterValue">The Setter that you want to set the <paramref name="control"/> to</param>
+        /// <param name="replace">Setting this will only set <paramref name="setterValue"/> if the property already exists on the <paramref name="control"/></param>
+        internal static void SetStyle(FrameworkElement control, Setter setterValue, bool replace = false)
+        {
+            try
+            {
+                Style newStyle = new Style(control.GetType(), GetStyle(control.Style));
+                if (!replace)
+                { newStyle.Setters.Add(setterValue); }
+                else
+                {
+                    SetterBaseCollection setterList = new SetterBaseCollection();
+                    Style tmpStyle = control.Style;
+                    do
+                    {
+                        foreach (var setter in tmpStyle.Setters.Cast<Setter>().Where(setter => !setterList.Contains(setter)))
+                        { setterList.Add(setter); }
+                        tmpStyle = tmpStyle.BasedOn;
+                    } while (tmpStyle != null);
+                    foreach (var setter in setterList.Cast<Setter>().Where(setter => setter.Property.Name.Equals(setterValue.Property.Name)))
+                    { newStyle.Setters.Add(setterValue); }
+                }
+                control.Style = newStyle;
+            }
+            catch (Exception ex) { throw new Exception(string.Format("Error Setting Style{0} - Control: {1}{0} - Style: {2}", Environment.NewLine, control, setterValue.Property + " = " + setterValue.Value), ex); }
+        }
+
+        /// <summary>
+        /// This iterates through the BasedOn styles and gets the first Base Style with Setters
+        /// </summary>
+        /// <remarks>
+        /// Miguel Santana 2015/04/06
+        /// </remarks>
+        /// <param name="style">Top Level Style</param>
+        /// <returns>Parent Style</returns>
+        private static Style GetStyle(Style style)
+        {
+            try
+            { return (style == null || style.Setters.Count > 0) ? style : GetStyle(style.BasedOn); }
+            catch (Exception ex) { throw new Exception("Error Getting Style", ex); }
         }
     }
 }
