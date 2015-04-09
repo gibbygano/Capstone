ed78fd8fef23e103f986d026666b16675aca7290
RoseS <rosesteffensmeier@yahoo.com>
2015-04-09 13:21:17 -0500

    Rose - added new test for SupplierLogin

--- com.WanderingTurtle/com.WanderingTurtle.Tests/SupplierLoginManagerTest.cs
+++ com.WanderingTurtle/com.WanderingTurtle.Tests/SupplierLoginManagerTest.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using com.WanderingTurtle.DataAccess;
+using com.WanderingTurtle.BusinessLogic;
 using com.WanderingTurtle.Common;
 using System.Data.SqlClient;
 
 namespace com.WanderingTurtle.Tests
 {
     [TestClass]
-    public class SupplierLoginAccessorTest
+    public class SupplierLoginManagerTest
     {
-        private SupplierLoginAccessor access = new SupplierLoginAccessor();
-        private SupplierLogin retrieveSupplier;
+        private SupplierLoginManager access = new SupplierLoginManager();
+        private SupplierLoginAccessorTest testing = new SupplierLoginAccessorTest();
+        private SupplierLogin retrieveSupplier = new SupplierLogin();
 
         [TestInitialize]
         public void initialize()
@@ -19,28 +20,28 @@ namespace com.WanderingTurtle.Tests
         }
 
         [TestMethod]
-        public void TestSupplierLoginGet()
+        public void TestSupplierLoginManageGet()
         {
             retrieveSupplier = access.retrieveSupplierLogin("Password#1", "F@k3logg3r");
             Assert.AreEqual("F@k3logg3r", retrieveSupplier.UserName);
         }
 
         [TestMethod]
-        public void TestSupplierLoginAdd()
+        public void testSupplierLoginManageAdd()
         {
             int numberAdded = access.addSupplierLogin("TryM3!");
             Assert.AreEqual("TryM3!", access.retrieveSupplierLogin("Password#1", "TryM3!").UserName);
         }
-        
+
         [TestMethod]
         [ExpectedException(typeof(SqlException))]
-        public void TestSupplierLoginAddFail()
+        public void TestSupplierLoginManageAddFail()
         {
             int numberAdded = access.addSupplierLogin("F@k3logg3r");
         }
 
         [TestMethod]
-        public void TestSupplierLoginArchive()
+        public void TestSupplierLoginManageArchive()
         {
             SupplierLogin change = access.retrieveSupplierLogin("Password#1", "F@k3logg3r");
             int num = access.archiveSupplierLogin(change, false);
@@ -48,29 +49,9 @@ namespace com.WanderingTurtle.Tests
         }
 
         [TestCleanup]
-        public void cleanUp()
+        public void cleanup()
         {
-            var conn = DatabaseConnection.GetDatabaseConnection();
-            string commandText = @"DELETE FROM SupplierLogin WHERE UserName = 'F@k3logg3r'";
-            string commandText2 = @"DELETE FROM SupplierLogin WHERE UserName='TryM3!'";
-
-            var cmd = new SqlCommand(commandText, conn);
-
-            try
-            {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                cmd = new SqlCommand(commandText2, conn);
-                cmd.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine("Fail!");
-            }
-            finally
-            {
-                conn.Close();
-            }
+            testing.cleanUp();
         }
     }
 }
