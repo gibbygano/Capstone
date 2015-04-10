67b81379e3198bfa8240a6be0030dad0c22aab7f
mlapka <matt.lapka@gmail.com>
2015-04-10 12:10:29 -0500

    add login bar ml

--- com.WanderingTurtle/com.WanderingTurtle.Web/Site.Master.designer.cs
+++ com.WanderingTurtle/com.WanderingTurtle.Web/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace com.WanderingTurtle.Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// LoginBar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::com.WanderingTurtle.Web.Pages.LoginBar LoginBar;
+        
+        /// <summary>
         /// Menu1 control.
         /// </summary>
         /// <remarks>
