0743c9216acdcca5646e024d58da67bf07121aae
mlapka <matt.lapka@gmail.com>
2015-04-14 19:17:10 -0500

    added Booking Numbers class and SP ml

---
+++ com.WanderingTurtle/com.WanderingTurtle.Common/BookingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.WanderingTurtle.Common
+{
+    public class BookingNumbers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Quantity { get; set; }
+        public int Room { get; set; }
+
+        public BookingNumbers()
+        {
+
+        }
+    }
+}
