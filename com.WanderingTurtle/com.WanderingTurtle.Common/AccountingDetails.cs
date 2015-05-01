e4e7ef40da5bfd35fae3922fa0635192dda0b830
Arik Chadima <thechad@southslope.net>
2015-05-01 04:06:22 -0500

    xml framework for accounting details, and accounting details for service

---
+++ com.WanderingTurtle/com.WanderingTurtle.Common/AccountingDetails.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace com.WanderingTurtle.Common
+{
+    public class AccountingDetails : IXMLAble
+    {
+        public List<AccountingInvoiceDetails> Invoices { get; private set; }
+        public List<AccountingSupplierListingDetails> SupplierListings { get; private set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+        public AccountingDetails()
+        {
+            Invoices = new List<AccountingInvoiceDetails>();
+            SupplierListings = new List<AccountingSupplierListingDetails>();
+
+        }
+
+        /// <summary>
+        /// Arik Chadima
+        /// Created: 2015/4/30
+        /// 
+        /// Serializes object into an xml string.
+        /// 
+        /// Adapted from code originally by Jim Glasgow
+        /// </summary>
+        /// <returns>string xml representation of object</returns>
+        public string ToXML()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(AccountingDetails));
+
+            MemoryStream memStream = new MemoryStream();
+
+            serializer.Serialize(memStream, this);
+
+            //we need a streamReader to read a memory stream, as it is just a buffer.
+
+            StreamReader reader = new StreamReader(memStream);
+
+            // read stream to a string.
+            memStream.Position = 0;
+            string text = reader.ReadToEnd();
+
+            reader.Close();
+            memStream.Close();
+
+            return text;
+        }
+
+        /// <summary>
+        /// Arik Chadima
+        /// Created: 2015/4/30
+        /// 
+        /// Saves a seriealized xml reperesentation of this object.
+        /// 
+        /// Adapted from code originally by Jim Glasgow
+        /// </summary>
+        /// <param name="filename">string name of the file and path to save the xml</param>
+        /// <returns>bool successful</returns>
+        public bool XMLFile(string filename)
+        {
+            try
+            {
+                //first, new up an xml serializer of the current type.
+                XmlSerializer serializer = new XmlSerializer(typeof(AccountingDetails));
+
+                //to write to a file, build a streamWriter
+                StreamWriter writer = new StreamWriter(filename);
+
+                serializer.Serialize(writer, this);
+
+                writer.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+
+    public class AccountingInvoiceDetails : InvoiceDetails
+    {
+        public List<BookingDetails> Bookings { get; set; }
+        public AccountingInvoiceDetails()
+            : base()
+        {
+            Bookings = new List<BookingDetails>();
+        }
+    }
+
+    public class AccountingSupplierListingDetails: Supplier
+    {
+        public List<ItemListingDetails> Items { get; set; }
+        public Supplier Vendor { get; set; }
+
+        public AccountingSupplierListingDetails()
+            :base()
+        {
+            Items = new List<ItemListingDetails>();
+        }
+    }
+}
---
+++ com.WanderingTurtle/com.WanderingTurtle/AccountingManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.WanderingTurtle.Common;
+using com.WanderingTurtle.DataAccess;
+
+namespace com.WanderingTurtle.BusinessLogic
+{
+    class AccountingManager
+    {
+        /// <summary>
+        /// Arik Chadima
+        /// Created: 2015/5/1
+        /// Unimplemented method for assembling required data for an AccountingDetails object.
+        /// </summary>
+        /// <param name="Start">start date of invoices</param>
+        /// <param name="End">end date of invoices</param>
+        /// <returns>AccountingDetails with object data as requested by the params</returns>
+        public AccountingDetails GetAccountingDetails(DateTime Start, DateTime End)
+        {
+            throw new NotImplementedException("Method not implemented.");
+        }
+    }
+}
