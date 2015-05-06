3b604482285097e6ccec5ee7dee341717463edb2
Miguel Santana <santana1053@gmail.com>
2015-05-06 03:26:27 -0500

    Added FlipView

--- com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Views/SplashScreen.xaml.cs
+++ com.WanderingTurtle/com.WanderingTurtle.FormPresentation/Views/SplashScreen.xaml.cs
@@ -1,13 +1,23 @@
 ﻿using com.WanderingTurtle.BusinessLogic;
 using com.WanderingTurtle.Common;
 using com.WanderingTurtle.FormPresentation.Models;
+using com.WanderingTurtle.FormPresentation.Resources;
 using MahApps.Metro;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Image = System.Windows.Controls.Image;
 
 namespace com.WanderingTurtle.FormPresentation.Views
 {
@@ -17,14 +27,51 @@ namespace com.WanderingTurtle.FormPresentation.Views
     /// <remarks>Miguel Santana 2015/03/10</remarks>
     public partial class StartupScreen
     {
+        private System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="StartupScreen" /> class.
         /// </summary>
+        /// <exception cref="InvalidOperationException">The item to add already has a different logical parent. </exception>
+        /// <exception cref="InvalidOperationException">The collection is in ItemsSource mode.</exception>
         public StartupScreen()
         {
             Globals.UserToken = null;
             Application.Current.ChangeAppStyle(ThemeManager.GetAccent("Emerald"));
             InitializeComponent();
+            FlipView.HideControlButtons();
+            foreach (var image in SplashImageList)
+            {
+                FlipView.Items.Add(new Image { Source = image, Stretch = Stretch.UniformToFill });
+            }
+            myTimer.Tick += TimerEventProcessor;
+            myTimer.Interval = 10000;
+            myTimer.Start();
+        }
+
+        private void TimerEventProcessor(object sender, EventArgs e)
+        {
+            FlipView.SelectedIndex = Equals(FlipView.SelectedIndex + 1, FlipView.Items.Count)
+                ? 0
+                : FlipView.SelectedIndex + 1;
+        }
+
+        /// <exception cref="MissingManifestResourceException"><paramref name="tryParents" /> is true, no usable set of resources has been found, and there are no default culture resources. </exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="culture" /> parameter is null. </exception>
+        /// <exception cref="ArgumentException">The height or width of the bitmap is greater than <see cref="F:System.Int16.MaxValue" />.</exception>
+        /// <exception cref="Exception">The operation failed.</exception>
+        /// <exception cref="InvalidCastException" accessor="get">An element in the sequence cannot be cast to type <paramref name="TResult" />.</exception>
+        public List<BitmapSource> SplashImageList
+        {
+            get
+            {
+                return SplashImages.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true)
+                    .Cast<DictionaryEntry>()
+                    .Where(resource => resource.Value is Bitmap)
+                    .Reverse()
+                    .Select(resource => ((Bitmap)resource.Value).ToBitmapSource())
+                    .ToList();
+            }
         }
 
         /// <summary>
@@ -64,7 +111,7 @@ namespace com.WanderingTurtle.FormPresentation.Views
             {
                 throw new InputValidationException(TxtPassword, ex, "Login Error");
             }
-              
+
             if (Globals.UserToken == null)
             {
                 throw new WanderingTurtleException(this, "Error setting User Token");
@@ -73,6 +120,7 @@ namespace com.WanderingTurtle.FormPresentation.Views
             var window = this.GetVisualParent<MainWindow>();
             window.BtnSignOut.Visibility = Visibility.Visible;
             window.MainContent.Content = new TabContainer();
+            myTimer.Stop();
         }
 
         private void TxtInput_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
